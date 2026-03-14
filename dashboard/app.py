d822a4bc160fa6d6e958160f2e0e9a3f7044d8d0
Hoan Hung Nim <hhoan2505@gmail.com>
2026-03-14 22:37:51 +0700

    fix: replace histogram to box plot

--- dashboard/app.py
+++ dashboard/app.py
@@ -847,7 +847,7 @@ with tab2:
                         line_color=color,
                         fillcolor=f"rgba({int(color[1:3],16)},{int(color[3:5],16)},{int(color[5:7],16)},0.4)",
                         boxmean=True,
-                        boxpoints=False,  # ẩn outlier points
+                        boxpoints=True,  # ẩn outlier points
                         marker=dict(size=4, opacity=0.6),
                     ))
             
